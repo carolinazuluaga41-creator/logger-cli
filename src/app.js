3fa0bf1364fc3b8b9dacdfc9a5f3aebd7eb38b45
Carolina Zuluaga <Carolinazuluaga41@gmail.com>
2025-09-06 09:39:38 -0500

    commit

--- src/app.js
+++ src/app.js
@@ -1,35 +1,31 @@
-// src/app.js
 
 import chalk from 'chalk';
 
 console.log(chalk.green.bold("=== Inicio del sistema ===\n"));
 
-console.time(chalk.yellow('⏱️ ProcesoPrincipal'));
+console.time(chalk.yellow('⏱ ProcesoPrincipal'));
 
 function accesoUsuario(usuario) {
   console.count(chalk.blue(`👤 Acceso de usuario ${usuario}`));
 }
 
-// Simulando accesos
+accesoUsuario('Carolina');
 accesoUsuario('Kevin');
-accesoUsuario('Paulina');
-accesoUsuario('Alexa');
+accesoUsuario('Mariana');
 
-// Advertencia
-console.warn(chalk.redBright('⚠️  Capacidad de usuarios alcanzando el límite'));
+console.warn(chalk.redBright('  Capacidad de usuarios alcanzando el límite'));
 
-// Error
-console.error(chalk.red.bold('❌ Error: No se pudo conectar a la base de datos'));
 
-// Mostrar usuarios como tabla
+console.error(chalk.red.bold(' Error: No se pudo conectar a la base de datos'));
+
 const usuarios = [
-  { nombre: "Kevin", rol: "Admin" },
-  { nombre: "Paulina", rol: "User" }
+  { nombre: "Carolina", rol: "Admin" },
+  { nombre: "Mariana", rol: "User" }
 ];
 
-console.log(chalk.cyan('\n📋 Lista de usuarios conectados:'));
+console.log(chalk.cyan('\n Lista de usuarios conectados:'));
 console.table(usuarios);
 
-console.timeEnd(chalk.yellow('⏱️ ProcesoPrincipal'));
+console.timeEnd(chalk.yellow('⏱ ProcesoPrincipal'));
 
 console.log(chalk.green.bold("\n=== Fin del sistema ==="));
